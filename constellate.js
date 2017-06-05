6cc445750ce6fc3d7c4851e106c7fd871aaf156a
Sean Matheson <sean@ctrlplusb.com>
2017-06-05 11:03:39 +0100

    Compile all projects.

--- constellate.js
+++ constellate.js
@@ -1,7 +1,17 @@
 module.exports = {
   projects: {
     constellate: {
+      compiler: 'babel',
       dependencies: ['constellate-dev-utils'],
+    },
+    'constellate-dev-utils': {
+      compiler: 'babel',
+    },
+    'create-constellate-app': {
+      compiler: 'babel',
+    },
+    'eslint-config-constellate': {
+      compiler: 'babel',
     },
   },
 }
