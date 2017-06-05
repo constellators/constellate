8c645c4dc2470382cefb8a57f49ef5b42e4f92ca
Sean Matheson <sean@ctrlplusb.com>
2017-06-05 11:26:48 +0100

    Fixes.

--- constellate.js
+++ constellate.js
@@ -1,17 +1,7 @@
 module.exports = {
   projects: {
     constellate: {
-      compiler: 'babel',
       dependencies: ['constellate-dev-utils', 'eslint-config-constellate'],
-    },
-    'constellate-dev-utils': {
-      compiler: 'babel',
-    },
-    'create-constellate-app': {
-      compiler: 'babel',
-    },
-    'eslint-config-constellate': {
-      compiler: 'babel',
     },
   },
 }
