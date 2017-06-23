2004514bb7520d8c9db9500695caa985065009e2
Sean Matheson <sean@ctrlplusb.com>
2017-06-23 01:33:19 +0100

    Clean cmd now also kills package-lock.json files.

--- projects/constellate-dev-utils/modules/projects/cleanProjects.js
+++ projects/constellate-dev-utils/modules/projects/cleanProjects.js
@@ -5,6 +5,7 @@ module.exports = function cleanProjects(projects) {
   projects.forEach((project) => {
     if (fs.existsSync(project.paths.nodeModules)) {
       TerminalUtils.verbose(`Removing node_modules for ${project.name}`)
+      fs.removeSync(project.paths.packageLockJson)
       fs.removeSync(project.paths.nodeModules)
     }
   })
