ad170538fe3b2e7416624671ff2a1ff461ea6b0c
Sean Matheson <sean@ctrlplusb.com>
2017-07-18 13:49:33 +0100

    Links projects per new behaviour.

--- constellate.js
+++ constellate.js
@@ -1,52 +1,10 @@
 module.exports = {
-  publishing: {
-    npm: {
-      enabled: true,
-    },
-  },
   projects: {
-    'babel-plugin-inject-source-map-init': {
-      // no config
-    },
-    constellate: {
-      dependencies: ['constellate-dev-utils'],
-    },
-    'constellate-dev-utils': {
-      // no config
-    },
-    'constellate-dev-utils-webpack': {
-      dependencies: ['constellate-dev-utils'],
-    },
-    'constellate-plugin-compiler-babel': {
-      dependencies: ['babel-plugin-inject-source-map-init', 'constellate-dev-utils'],
-    },
-    'constellate-plugin-compiler-webpack': {
-      dependencies: ['constellate-dev-utils', 'constellate-dev-utils-webpack'],
-    },
-    'constellate-plugin-compiler-webpack-node': {
-      dependencies: ['constellate-dev-utils', 'constellate-dev-utils-webpack'],
-    },
-    'constellate-plugin-deploy-now': {
-      dependencies: ['constellate-dev-utils'],
-    },
-    'constellate-plugin-develop-webpack': {
-      dependencies: [
-        'constellate-plugin-compiler-webpack',
-        'constellate-dev-utils-webpack',
-        'constellate-dev-utils',
-      ],
-    },
     'constellate-utils': {
       compiler: 'babel',
       compilerOptions: {
         nodeVersion: '4.8.3',
       },
-    },
-    'create-constellate-app': {
-      // no config
-    },
-    'eslint-config-constellate': {
-      // no config
     },
   },
 }
